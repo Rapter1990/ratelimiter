c396e5b44960bc014ab3716c462e1ab5638d070b
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-06-28 23:19:34 +0300

    Task 1 : Initial commit

---
+++ src/main/java/com/springboot/ratelimiter/RatelimiterApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.ratelimiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatelimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatelimiterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/ratelimiter/RatelimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.ratelimiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatelimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
