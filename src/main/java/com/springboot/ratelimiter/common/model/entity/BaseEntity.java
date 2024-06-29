fa1ad62aadcdd5b9e5e9e7523eff51c8612d056c
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-06-29 19:20:38 +0300

    Task 15 : Revise BaseEntity

--- src/main/java/com/springboot/ratelimiter/common/model/entity/BaseEntity.java
+++ src/main/java/com/springboot/ratelimiter/common/model/entity/BaseEntity.java
@@ -19,19 +19,13 @@ import java.util.Optional;
 @MappedSuperclass
 @NoArgsConstructor
 @AllArgsConstructor
-public abstract class BaseDomainModel {
+public abstract class BaseEntity {
 
     @Column(name = "CREATED_AT")
     private LocalDateTime createdAt;
 
-    @Column(name = "CREATED_BY")
-    private String createdBy;
-
     @Column(name = "UPDATED_AT")
     private LocalDateTime updatedAt;
-
-    @Column(name = "UPDATED_BY")
-    private String updatedBy;
 
     @PrePersist
     public void prePersist() {
