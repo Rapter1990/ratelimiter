5791a788530e47ea130db341537b254376a44a3f
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-06-29 19:23:59 +0300

    Task 17 : Define CacheConfig and RedisConfig

---
+++ src/main/java/com/springboot/ratelimiter/config/CacheConfig.java
@@ -0,0 +1,11 @@
+package com.springboot.ratelimiter.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+}
+
---
+++ src/main/java/com/springboot/ratelimiter/config/RedisConfig.java
@@ -0,0 +1,29 @@
+package com.springboot.ratelimiter.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
+
+}
+
